9457cb02adfd994f9212e3fc6c788e7833d0a2fa
Emad-Ayad <emadayad228@gmail.com>
2025-02-10 00:09:36 +0200

    fix un necessary scrolling in signUp view

--- lib/features/auth/presentaion/view/sign_up_view.dart
+++ lib/features/auth/presentaion/view/sign_up_view.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:honey_comp/core/services/get_it_service.dart';
+import 'package:honey_comp/core/widgets/build_snack_bar.dart';
 import 'package:honey_comp/features/auth/domain/repos/auth_repo.dart';
 import 'package:honey_comp/features/auth/presentaion/cubits/signup_cubit/signup_cubit.dart';
 import 'package:honey_comp/features/auth/presentaion/view/widgets/sign_up_view_body.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class SignUpView extends StatelessWidget {
   const SignUpView({super.key});
@@ -14,7 +16,31 @@ class SignUpView extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => SignupCubit(getIt<AuthRepo>()),
-      child: const SignUpViewBody(),
+      child: const SignUpBlocConsumer(),
+    );
+  }
+}
+
+class SignUpBlocConsumer extends StatelessWidget {
+  const SignUpBlocConsumer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<SignupCubit, SignupState>(
+      listener: (context, state) {
+        if (state is SignupSuccess) {
+          buildSnackBar(context, "SignUp Done Successfully");
+        }
+        if (state is SignupFailure) {
+          buildSnackBar(context, state.message);
+        }
+      },
+      builder: (context, state) {
+        return ModalProgressHUD(
+            inAsyncCall: state is SignupLoading, child: const SignUpViewBody());
+      },
     );
   }
 }
--- lib/features/auth/presentaion/view/widgets/sign_up_view_body.dart
+++ lib/features/auth/presentaion/view/widgets/sign_up_view_body.dart
@@ -9,8 +9,6 @@ import 'package:honey_comp/core/widgets/custom_button.dart';
 import 'package:honey_comp/core/widgets/custom_text_form_field.dart';
 import 'package:honey_comp/features/auth/presentaion/cubits/signup_cubit/signup_cubit.dart';
 import 'package:honey_comp/generated/l10n.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
-
 import '../../../../../core/widgets/password_field.dart';
 
 class SignUpViewBody extends StatefulWidget {
@@ -31,119 +29,101 @@ class _SignUpViewBodyState extends State<SignUpViewBody> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: buildAppBar(context, title: S.of(context).newAccount),
-      body: BlocConsumer<SignupCubit, SignupState>(
-        listener: (context, state) {
-          if (state is SignupSuccess) {}
-          if (state is SignupFailure) {
-            buildSnackBar(context, state.message);
-          }
-        },
-        builder: (context, state) {
-          return SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 14.0),
-              child: SizedBox(
-                height: MediaQuery.sizeOf(context).height,
-                child: ModalProgressHUD(
-                  inAsyncCall: state is SignupLoading,
-                  child: Form(
-                    key: globalKey,
-                    autovalidateMode: autoValidateMode,
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        const SizedBox(height: 24),
-                        CustomTextFormField(
-                            hintText: S.of(context).fullName,
-                            onSaved: (value) {
-                              name = value!;
-                            },
-                            keyboardType: TextInputType.name),
-                        const SizedBox(height: 16),
-                        CustomTextFormField(
-                            hintText: S.of(context).email,
-                            onSaved: (value) {
-                              email = value!;
-                            },
-                            keyboardType: TextInputType.emailAddress),
-                        const SizedBox(height: 16),
-                        PasswordField(
-                          onSaved: (value) {
-                            password = value!;
-                          },
-                        ),
-                        const SizedBox(height: 10),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Checkbox(
-                                value: isChecked,
-                                onChanged: (value) {
-                                  setState(() {
-                                    isChecked = value!;
-                                    log(value.toString());
-                                  });
-                                }),
-                            Expanded(
-                              child: Text(
-                                S.of(context).terms,
-                                maxLines: 2,
-                                overflow: TextOverflow.visible,
-                                style: AppTextStyles.bodyText2,
-                              ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 33),
-                        CustomButton(
-                            onPressed: () {
-                              if (globalKey.currentState!.validate()) {
-                                if (isChecked) {
-                                  globalKey.currentState!.save();
-                                  context
-                                      .read<SignupCubit>()
-                                      .createUserWithEmailAndPassword(
-                                          email: email,
-                                          password: password,
-                                          name: name);
-                                } else {
-                                  buildSnackBar(context,
-                                      'الرجاء الموافقة علي الشروط والاحكام');
-                                }
-                              } else {
-                                setState(() {
-                                  autoValidateMode = AutovalidateMode.always;
-                                });
-                              }
-                            },
-                            title: S.of(context).signUp),
-                        const SizedBox(height: 33),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              S.of(context).haveAccount,
-                              style: AppTextStyles.bodyText1
-                                  .copyWith(color: const Color(0xff949D9E)),
-                            ),
-                            GestureDetector(
-                              onTap: () => Navigator.pop(context),
-                              child: Text(
-                                S.of(context).login,
-                                style: AppTextStyles.bodyText1
-                                    .copyWith(color: const Color(0xff1B5E37)),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 14.0),
+          child: Form(
+            key: globalKey,
+            autovalidateMode: autoValidateMode,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const SizedBox(height: 24),
+                CustomTextFormField(
+                    hintText: S.of(context).fullName,
+                    onSaved: (value) {
+                      name = value!;
+                    },
+                    keyboardType: TextInputType.name),
+                const SizedBox(height: 16),
+                CustomTextFormField(
+                    hintText: S.of(context).email,
+                    onSaved: (value) {
+                      email = value!;
+                    },
+                    keyboardType: TextInputType.emailAddress),
+                const SizedBox(height: 16),
+                PasswordField(
+                  onSaved: (value) {
+                    password = value!;
+                  },
                 ),
-              ),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Checkbox(
+                        value: isChecked,
+                        onChanged: (value) {
+                          setState(() {
+                            isChecked = value!;
+                            log(value.toString());
+                          });
+                        }),
+                    Expanded(
+                      child: Text(
+                        S.of(context).terms,
+                        maxLines: 2,
+                        overflow: TextOverflow.visible,
+                        style: AppTextStyles.bodyText2,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 33),
+                CustomButton(
+                    onPressed: () {
+                      if (globalKey.currentState!.validate()) {
+                        if (isChecked) {
+                          globalKey.currentState!.save();
+                          context
+                              .read<SignupCubit>()
+                              .createUserWithEmailAndPassword(
+                                  email: email, password: password, name: name);
+                        } else {
+                          buildSnackBar(
+                              context, 'الرجاء الموافقة علي الشروط والاحكام');
+                        }
+                      } else {
+                        setState(() {
+                          autoValidateMode = AutovalidateMode.always;
+                        });
+                      }
+                    },
+                    title: S.of(context).signUp),
+                const SizedBox(height: 33),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      S.of(context).haveAccount,
+                      style: AppTextStyles.bodyText1
+                          .copyWith(color: const Color(0xff949D9E)),
+                    ),
+                    GestureDetector(
+                      onTap: () => Navigator.pop(context),
+                      child: Text(
+                        S.of(context).login,
+                        style: AppTextStyles.bodyText1
+                            .copyWith(color: const Color(0xff1B5E37)),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-          );
-        },
+          ),
+        ),
       ),
     );
   }
